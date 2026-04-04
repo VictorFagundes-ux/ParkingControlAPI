3f930815cf29064c1ec541cd75959b2132592bad
VictorFagundes-ux <victorsorrentin@gmail.com>
2026-04-03 22:54:02 -0300

    implementação do controller, DTO,Repository, Service,DataConfig e Model

---
+++ Springboot.ParkingControl/src/test/java/VictorFagundes_ux/ParkingControlAPI/Springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package VictorFagundes_ux.ParkingControlAPI.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
